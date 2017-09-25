ed237a72ac0f8d6dbf9197fa2b0578b00835b07e
wflying000 <wf123580@163.com>
2017-09-25 15:39:39 +0800

    Update slamBase.cpp

--- src/slamBase.cpp
+++ src/slamBase.cpp
@@ -9,7 +9,21 @@ PointCloud::Ptr image2PointCloud(cv::Mat &rgb, cv::Mat &depth, CAMERA_INTRINSIC_
             ushort d = depth.ptr<ushort>(m)[n];
             if(d<=0)
                 continue;
+            PointT p;
+            p.z = double(d) / camera.scale;
+            p.x = (n - camera.cx) * p.z / camera.fx;
+            p.y = (m - camera.cy) * p.z / camera.fy;
+            
+            p.b = rgb.ptr<uchar>(m)[n*3];
+            p.g = rgb.ptr<uchar>(m)[n*3+1];
+            p.r = rgb.ptr<uchar>(m)[n*3+2];
+            
+            cloud->push_back(p);
                 
        }
     }
+    cloud->height = 1;
+    cloud->width = cloud->points.size();
+    cloud->is_dense = false;
+    return cloud;
 }
