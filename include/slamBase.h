565c644a5482c1d1e08c47b8b8095e367329f59a
wflying000 <wf123580@163.com>
2017-09-25 15:27:09 +0800

    Update slamBase.h

--- include/slamBase.h
+++ include/slamBase.h
@@ -3,3 +3,43 @@
 using namespace std;
 
 #include<Eigen/Core>
+#include<Eigen/Geometry>
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/hihgui/highgui.hpp>
+#include<opencv2/calib3d/calib3d.hpp>
+#include<opencv2/core/eigen.hpp>
+
+#include<pcl/io/pcl_io.h>
+#include<pcl/point_types.h>
+#include<pcl/common/transforms.h>
+#include<pcl/visualization/cloud_viewer.h>
+#include<pcl/filters/voxel_grid.h>
+
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+
+struct CAMERA_INTRINSIC_PARAMETERS{
+  double cx, cy, fx, fy, scale;
+};
+
+struct Frame{
+  int frameID;
+  cv::Mat rgb, depth;
+  cv::Mat Desp;
+  vector<cv::KeyPoint> keypoint;
+};
+
+struct RESULT_OF_PNP{
+  cv::Mat rvec, tvec;
+  int inliers;
+};
+
+
+PointCloud::Ptr image2PointCloud(cv::Mat &rgb, cv::Mat &depth, CAMERA_INTRINSIC_PARAMETERS camera);
+
+
+
+
+
+
